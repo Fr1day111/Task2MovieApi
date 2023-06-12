e5198ebff44ee341bf94cdc832cacba9b3905d31
Fri Day <nwrsudeep@gmail.com>
2023-06-12 21:01:16 +0545

    Final

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tasksecond/API.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +11,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List movies=[];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    loadmovies();
+  }
+  loadmovies() async{
+    TMDB tmdbwithCustomLogs= TMDB(ApiKeys(dotenv.env['APIKey']!,dotenv.env['ReadAccessToken']!),
+      logConfig: ConfigLogger(
+        showLogs: true,//must be true than only all other logs will be shown
+        showErrorLogs: true,
+      ),
+    );
+    Map popular = await tmdbwithCustomLogs.v3.movies.getPopular();
+    setState(() {
+      movies=popular['results'];
+    });
+    print(movies);
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        height: 100,
+        child: Column(
+          children: [
+            Text('Popular'),
+            ListView.builder(
+                itemCount: movies.length,
+                itemBuilder: (context,index){
+                return InkWell(
+                  onTap: (){},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'http://image.tmdb.org./t/p/w500'+movies[index]['poster_path']
+                        )
+                      )
+                    ),
+                  ),
+                );
+            }),
+          ],
+        ),
+      )
+    );
   }
 }
